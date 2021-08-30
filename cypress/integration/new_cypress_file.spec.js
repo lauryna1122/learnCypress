22f893110798fd0312cce2094df7bb9d217be4c5
lauryna1122 <70980577+lauryna1122@users.noreply.github.com>
2021-08-30 23:31:01 +0300

    first tes, doesn't work

--- cypress/integration/new_cypress_file.spec.js
+++ cypress/integration/new_cypress_file.spec.js
@@ -1,1 +1,8 @@
-describe('mY first test')
+const { expect } = require("chai")
+const { it } = require("mocha")
+
+describe('mY first test', () => {
+    it('does not much', ()=>{
+        expect(true).to.equal(true)
+    })
+})
